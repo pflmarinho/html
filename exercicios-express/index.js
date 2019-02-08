c8586d70c480c75500dcaf5ecb81d8e492607618
pflmarinho <pflmarinho@gmail.com>
2019-02-08 14:23:24 -0200

    Projeto de React 08/02 new

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -3,10 +3,17 @@ const app = express()
 
 app.get(("/opa", (req, res) => {
     res.json([
-        {id: 7, name:'ana', position:1},
-        {ide: 34, name:'bia', position:2},
-        {id: 73,name:'calos', position:3}
-    ])
+        data: [
+            {id: 7, name:'ana', position:1},
+            {id: 34, name:'bia', position:2},
+            {id: 73, name:'calos', position:3}
+        ],
+        count:30,
+        skip: 0,
+        limit:3,
+        status:200;
+        
+    })
 
 app.listen(3000, () => {
     console.log("backend executando...")
